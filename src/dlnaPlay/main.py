de7d65653e0d0b169e47bb51a12223504c6a9c52
Ryan <ryan@sffzh.cn>
2026-01-12 15:29:53 +0800

    * 调整日志文本提示

--- src/dlnaPlay/main.py
+++ src/dlnaPlay/main.py
@@ -444,7 +444,7 @@ def _init_start_volume(device:DLNADevice, origin_volume:int, volume_target:int, 
 # 睡眠定时器：此时间后自动退出播放
 def _set_sleep_counter(stop_playing_after:float):
     if not stop_playing_after:
-        logger.info("未设置休眠时长，不会主动退出。")
+        logger.info("未设置休眠时长。")
         return None
     def timeout_handler():
         logger.info('达到设置的最大播放时长: [%f] 秒，停止播放。', stop_playing_after)
